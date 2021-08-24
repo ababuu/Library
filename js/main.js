33fabcda8e73b9e9a8aa0a533d4642b5fbef74e8
ababuu <ababuu@github.com>
2021-08-24 09:34:23 +0300

    changed-to-class

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-const section=document.querySelector('.cards');
+document.querySelector('.openButton').addEventListener('click', ()=>{
+    const open=new openForm();
+    open.open();
+})
 
+document.querySelector('.cancel').addEventListener('click', ()=>{
+    const close=new closeForm();
+    close.close();
+})
+
+        
 const submitBtn=document.querySelector('.submit');
-let initiallyStored=[];
+
 let myLibrary=[];
-//let i=0;
-let n=0;
+
 
 
 
@@ -17,56 +25,66 @@ submitBtn.addEventListener('click', e => {
                     document.getElementById('status').value)
 
 
-    myLibrary.push(initiallyStored);
+    myLibrary.pop();
     myLibrary.push(book);
     console.log(myLibrary);
     //console.log(i);
     
-    localStorage.setItem('Book List',JSON.stringify(myLibrary));
-    console.log(JSON.parse(localStorage.getItem('Book List')));
+    // localStorage.setItem('Book List',JSON.stringify(myLibrary));
+    //console.log(JSON.parse(localStorage.getItem('Book List')));
     
     
-    
-    displayBook(JSON.parse(localStorage.getItem('Book List')));
-    closeForm();
+    const display=new displayBook(myLibrary)
+    display.display();
+    const close=new closeForm();
+    close.close();
     document.querySelector('form').reset();
-    n++;
+    
+
 });
 
 
 
-function Books(author,title,pages,status){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.status=status;
 
-    this.info= function(){
+class Books{
+
+    constructor(author,title,pages,status){
+
+        this.title=title;
+        this.author=author;
+        this.pages=pages;
+        this.status=status;
+    }
+    info(){
         return `${title} by ${author}, ${pages} pages, ${status}`;
     }
 }
 
 
 
-function displayBook(array){
+class displayBook{
 
-    //array=JSON.parse(localStorage.getItem('Book List'));
+    constructor(array){
+        this.array=array;
+    }
         
-        let card=document.createElement('article');
-        let author=document.createElement('h3');
-        let title=document.createElement('h4');
-        let pages=document.createElement('h4');
-        let status=document.createElement('h4');
-        let deleteBtn=document.createElement('img');
-        let switchToggle=document.createElement('label');
-        let input=document.createElement('input');
+        
+        display(){
+            
+
+        for(let i=0; i<this.array.length; i++){
+            let author=document.createElement('h3');
+            let title=document.createElement('h4');
+            let pages=document.createElement('h4');
+            let status=document.createElement('h4');
+            let deleteBtn=document.createElement('img');
+            const section=document.querySelector('.cards');
+let card=document.createElement('article');
+            let switchToggle=document.createElement('label');
+            let input=document.createElement('input');
+            deleteBtn.src='https://cdn4.iconfinder.com/data/icons/social-messaging-ui-coloricon-1/21/52-512.png'
         input.type='checkbox';
         let span=document.createElement('span');
-
-        deleteBtn.src='https://cdn4.iconfinder.com/data/icons/social-messaging-ui-coloricon-1/21/52-512.png'
-
-
-        for(let i=0; i<array.length; i++){
         card.classList.add('card',`card-0${i}`);
         author.classList.add(`author-0${i}`);
         title.classList.add(`title-0${i}`);
@@ -74,10 +92,10 @@ function displayBook(array){
         switchToggle.classList.add('switch');
         span.classList.add('slider', 'round');
         
-        author.textContent=`Author: ${array[i]['author']}`;
-        title.textContent=`Title: ${array[i]['title']}`;
-        pages.textContent=`Pages: ${array[i]['pages']}`;
-        status.textContent=`Status: ${array[i]['status']}`;
+        author.textContent=`Author: ${this.array[i]['author']}`;
+        title.textContent=`Title: ${this.array[i]['title']}`;
+        pages.textContent=`Pages: ${this.array[i]['pages']}`;
+        status.textContent=`Status: ${this.array[i]['status']}`;
 
         switchToggle.appendChild(input);
         switchToggle.appendChild(span);
@@ -89,7 +107,7 @@ function displayBook(array){
         card.appendChild(status);
         card.appendChild(deleteBtn);
         card.appendChild(switchToggle);
-        }
+
 
         deleteBtn.addEventListener('click',e=>{
             let btn=e.target;
@@ -110,37 +128,47 @@ function displayBook(array){
                 
                 }
             });
+            
+        }
+
         
+        }
         //i++;
 }
 
 
 
-function openForm() {
+class openForm {
+    open(){
     document.getElementById("popupForm").style.display = "block";
-}
-function closeForm() {
-    document.getElementById("popupForm").style.display = "none";
-}
-
-function load(){
-    let storedValue= JSON.parse(localStorage.getItem('Book List'));
-    
-    if (storedValue!=null){
-        initiallyStored.push(storedValue);
-        let storedLength=storedValue.length;
-        
-        for(let x=0; x<storedLength; x++){
-            
-            displayBook(storedValue);
-            
-        }
-        
-        
     }
-    //i--;
-    //console.log(i);
+}
+class closeForm {
+    close(){
+        document.getElementById("popupForm").style.display = "none";
+}
     
 }
 
-window.onload=load();
+// function load(){
+//     let storedValue= JSON.parse(localStorage.getItem('Book List'));
+    
+//     if (storedValue!=null){
+//         initiallyStored.push(storedValue);
+//         let storedLength=storedValue.length;
+        
+//         for(let x=0; x<storedLength; x++){
+            
+//             const display=new displayBook(storedValue);
+//             display.display();
+            
+//         }
+        
+        
+//     }
+//     //i--;
+//     //console.log(i);
+    
+// }
+
+// window.onload=load();
